39ccf179b1572ce2f70bddaaa9776cb2fbffdd38
maddrake220 <jwchoi4118@gmail.com>
2022-01-12 18:44:29 +0900

    기본적인 라우팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import Detail from "./pages/Detail";
+import Edit from "./pages/Edit";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Signin from "./pages/Signin";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
