ac6c10c0dbb1dbf226761d7aea1416613d6f4632
maddrake220 <jwchoi4118@gmail.com>
2022-01-13 16:55:57 +0900

    feat: logout

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,16 @@
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 import SigninContainer from "../containers/SigninContainer";
+import { RootState } from "../types";
 
 export default function Signin() {
+  const token = useSelector<RootState, string | null>(
+    (state) => state.auth.token
+  );
+
+  if (token !== null) {
+    return <Redirect to="/" />;
+  }
+
   return <SigninContainer />;
 }
--- src/redux/modules/auth.ts
+++ src/redux/modules/auth.ts
@@ -1,15 +1,9 @@
 import { push } from "connected-react-router";
 import { Action, createActions, handleActions } from "redux-actions";
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, select, takeEvery } from "redux-saga/effects";
 import TokenService from "../../services/TokenService";
 import UserService from "../../services/UserService";
-import { LoginReqType } from "../../types";
-
-interface AuthState {
-  token: string | null;
-  loading: boolean;
-  error: Error | null;
-}
+import { AuthState, LoginReqType } from "../../types";
 
 const initialState: AuthState = {
   token: null,
@@ -72,6 +66,20 @@ function* loginSaga(action: Action<LoginReqType>) {
     yield put(fail(new Error(error?.response?.data?.error || "UNKNOWN_ERROR")));
   }
 }
+
+function* logoutSaga() {
+  try {
+    yield put(pending());
+    const token: string = yield select((state) => state.auth.token);
+    yield call(UserService.logout, token);
+    TokenService.set(token);
+  } catch (error: any) {
+  } finally {
+    TokenService.remove();
+    yield put(success(null));
+  }
+}
 export function* authSaga() {
   yield takeEvery(`${prefix}/LOGIN`, loginSaga);
+  yield takeEvery(`${prefix}/LOGOUT`, logoutSaga);
 }
